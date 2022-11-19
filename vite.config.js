5a4761e19924c0c4937b69cbc16de7a4cc1e9ba8
Solomon Akuson <akusonsolomon15@gmail.com>
2022-11-19 02:04:19 +0100

    completed pwa

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,35 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "auto-update",
+      registerType: "autoUpdate",
       devOptions: {
         enabled: true,
       },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'BFA App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
     }),
   ],
 });
