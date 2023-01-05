2784af249a0e2ea0e0f5d6fa2e80c1fc6c5a1e20
Solomon Akuson <akusonsolomon15@gmail.com>
2023-01-05 22:05:06 +0100

    git push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import vid from "../assets/vid.mp4";
 
 const Header = () => {
   return (
-    <header className="flex 2xl:h-[70vh] h-screen relative top-[84px] bg-brand2 bg-cover">
+    <header className="flex 2xl:h-[70vh] h-[90vh] md:h-screen relative top-[84px] bg-brand2 bg-cover">
     <div className="w-full h-full absolute top-0 left-0 right-0 bg-brand2 opacity-[0.5]">
 
     </div>
@@ -14,7 +14,7 @@ const Header = () => {
       </video>
       <div className="absolute w-full h-full">
         <div className="flex flex-col h-full items-center justify-center max-w-[1070px] mx-auto">
-          <h1 className="font-mon text-center text-[#fff] mr-3 text-[42px] font-[500]">
+          <h1 className="font-mon text-center text-[#fff] mr-3 text-[20px] md:text-[42px] font-[500]">
           <span className="text-[40px] md:text-[70px] font-[700]">Beacon of Faith Academy</span> <br />
          A Place to Learn, Grow, and Thrive<br />
           </h1>
